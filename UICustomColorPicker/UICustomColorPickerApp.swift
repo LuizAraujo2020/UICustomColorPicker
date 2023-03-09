4ba54d72d1d6281224b1bc6f1f5211eca6e66d08
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 13:06:00 -0300

    Initial Commit

---
+++ UICustomColorPicker/UICustomColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICustomColorPickerApp.swift
+//  UICustomColorPicker
+//
+//  Created by Luiz Araujo on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UICustomColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
